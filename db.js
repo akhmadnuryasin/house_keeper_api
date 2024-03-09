1a0b3d95b246945c638568578d107c06ece31111
Akhmad Nuryasin <akhmadnuryasin18@gmail.com>
2024-03-09 22:26:04 +0700

    fixing

--- db.js
+++ db.js
@@ -3,8 +3,8 @@ const mysql = require('mysql2');
 
 const db = mysql.createConnection({
     host: 'localhost',
-    user: 'root', // Ganti dengan nama pengguna MySQL Anda
-    password: '', // Ganti dengan kata sandi MySQL Anda
+    user: 'root',
+    password: '',
     database: 'hotel_management',
 });
 
